24341e7bf81c77237e4ba747874c002ccbc70b1f
Pedro Reis <pedroreisv@gmail.com>
2016-09-24 22:57:26 -0300

    Improvements on jQuery code

--- js/selections.js
+++ js/selections.js
@@ -1,55 +1,56 @@
 $("h3.one-player").on("click", function() {
+  $("h3").removeClass("selected");
+
   $(this).toggleClass("selected");
-  $("h3.two-players").removeClass("selected");
   $("div.controller-options").toggleClass("hidden");
-  if (!$("div.two-players-controllers").hasClass("hidden")) {
-    $("div.two-players-controllers").addClass("hidden");
-  }
+
+  $("div.two-players-controllers").addClass("hidden");
+  $("div.start").addClass("hidden");
   pong.multiPlayer = false;
   pong.singlePlayer = true;
 });
 
 $("h3.two-players").on("click", function() {
+  $("h3").removeClass("selected");
+
   $(this).toggleClass("selected");
-  if (!$("div.keyboard").hasClass("hidden")) {
-    $("h3.keyboard-selector").removeClass("selected");
-    $("div.keyboard").addClass("hidden");
-    $("div.controller-options").addClass("hidden");
-  }
-  if (!$("div.mouse").hasClass("hidden")) {
-    $("h3.mouse-selector").removeClass("selected");
-    $("div.mouse").addClass("hidden");
-    $("div.controller-options").addClass("hidden");
-  }
-  $("h3.one-player").removeClass("selected");
+
+  $("div.keyboard").addClass("hidden");
+  $("div.controller-options").addClass("hidden");
+  $("div.mouse").addClass("hidden");
+
   $("div.two-players-controllers").toggleClass("hidden");
-  $("div.start").removeClass("hidden");
+  if ($("div.start").hasClass("hidden")) {
+    $("div.start").toggleClass("hidden");
+  }
   pong.multiPlayer = true;
   pong.singlePlayer = false;
 });
 
 $("h3.keyboard-selector").on("click", function() {
+  $("div.controller-options h3").removeClass("selected");
+  $("div.keyboard").addClass("hidden");
+  $("div.mouse").addClass("hidden");
   $(this).toggleClass("selected");
-  if ($("h3.mouse-selector").hasClass("selected")) {
-    $("h3.mouse-selector").removeClass("selected");
-  }
-  if (!$("div.mouse").hasClass("hidden")) {
-    $("div.mouse").addClass("hidden");
-  }
+
   $("div.keyboard").toggleClass("hidden");
-  $("div.start").removeClass("hidden");
+
+  if ($("div.start").hasClass("hidden")) {
+    $("div.start").toggleClass("hidden");
+  }
 });
 
 $("h3.mouse-selector").on("click", function() {
+  $("div.controller-options h3").removeClass("selected");
+  $("div.keyboard").addClass("hidden");
+  $("div.mouse").addClass("hidden");
   $(this).toggleClass("selected");
-  if ($("h3.keyboard-selector").hasClass("selected")) {
-    $("h3.keyboard-selector").removeClass("selected");
-  }
-  if (!$("div.keyboard").hasClass("hidden")) {
-    $("div.keyboard").addClass("hidden");
-  }
+
   $("div.mouse").toggleClass("hidden");
-  $("div.start").removeClass("hidden");
+
+  if ($("div.start").hasClass("hidden")) {
+    $("div.start").toggleClass("hidden");
+  }
 });
 
 $("h3.start-game").on("click", function() {
